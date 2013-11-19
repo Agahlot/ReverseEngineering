51efbe4f6323da78ed1017831e5a793d16e961d4
Another Spike's fan <tf.airane@gmail.com>
2013-11-19 18:34:22 +0100

    Update Kernel32 IAT.c

--- Kernel32 IAT.c
+++ Kernel32 IAT.c
@@ -37,8 +37,7 @@
             while(OrigThunk->u1.AddressOfData != 0)
             {
                     PIMAGE_IMPORT_BY_NAME APIName = (PIMAGE_IMPORT_BY_NAME)(hFileMapView + OrigThunk->u1.AddressOfData);
-                    if(APIName->Name < 0x80000000)
-                        printf("[~] API Name%s\n", APIName->Name);
+                        printf("[~] API Name%10s\n", APIName->Name);
                     OrigThunk++;
             }
             IATBase++;
