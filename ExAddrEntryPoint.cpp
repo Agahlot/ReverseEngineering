f3ae97245eddda7ee66fa94253ee7ca3f7393b18
AiraneToufik <tf.airane@gmail.com>
2014-01-04 05:55:39 +0100

    Break Entry Point, incoming malware tech for W7

---
+++ ExAddrEntryPoint.cpp
@@ -0,0 +1,51 @@
+/*\
+\ / Author ~ From   | Toufik Airane ~ Paris
+/ \ GitHub          | toufikairane@github.io
+\ / Mail to         | tf.airane@gmail.com
+/ \ Twitter         | @toufikairane
+\ /
+/ \ Source file     | ExAddrEntryPoint.cpp
+\ / Language        | C++ ~ Windows API
+/ \ Brief           | Rewrite AddressOfEntryPoint
+\ /
+/ \ Licence :   	| Cette oeuvre est totalement libre de droit.
+\ /      			| Je vous encourage à la partager et/ou la modifier.
+/ \    				| Son utilisation engage votre entière responsabilité.
+\*/
+
+    #include <iostream>
+    #include <windows.h>
+    using namespace std;
+
+    int main(int argc, char *argv[])
+    {
+        if(argc!=2)
+            exit(EXIT_FAILURE);
+
+        HANDLE hFile = CreateFile( argv[1],
+                                   GENERIC_WRITE|GENERIC_READ,
+                                   FILE_SHARE_WRITE|FILE_SHARE_READ,
+                                   NULL,
+                                   OPEN_EXISTING,
+                                   FILE_ATTRIBUTE_NORMAL,
+                                   NULL );
+
+        DWORD dwTaille = 0;
+        IMAGE_DOS_HEADER hDOS;
+        IMAGE_NT_HEADERS hNT;
+
+        SetFilePointer(hFile, 0, 0, FILE_BEGIN);
+        ReadFile(hFile, &hDOS, sizeof(IMAGE_DOS_HEADER), &dwTaille, NULL);  // DOS HEADER
+
+        SetFilePointer(hFile, hDOS.e_lfanew, 0, FILE_BEGIN);
+        ReadFile(hFile, &hNT, sizeof(IMAGE_NT_HEADERS), &dwTaille, NULL);   // NT HEADER
+
+        cout << hex << "Ex AddressOfEntryPoint Offset : " << hNT.OptionalHeader.AddressOfEntryPoint << endl;
+        hNT.OptionalHeader.AddressOfEntryPoint = 0x00001337;                // HARDCODE OFFSET ENTRY POINT
+        SetFilePointer(hFile, hDOS.e_lfanew, 0, FILE_BEGIN);
+        WriteFile(hFile, &hNT, sizeof(hNT), &dwTaille, NULL);               // Patch NT HEADER
+        cout << hex << "New  AddressOfEntryPoint Offset : " << hNT.OptionalHeader.AddressOfEntryPoint << endl;
+
+        CloseHandle(hFile);
+        return 0;
+    }
