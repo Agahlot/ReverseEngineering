4487bdda8f36b7dc738abd64b771a259041ba840
AiraneToufik <tf.airane@gmail.com>
2014-02-25 00:29:04 +0100

    InjectDll

--- InjectDLL/inject.cpp
+++ InjectDLL/inject.cpp
@@ -1,119 +1,109 @@
-/*\
-\ / Author ~ From   | Toufik Airane ~ Paris
-/ \ GitHub          | tfairane.github.com
-\ / Mail            | tf.airane@gmail.com
-/ \ Twitter         | @tfairane
-\ /
-/ \ File            | injectDLL.cpp
-\ / Language        | C++
-/ \ Brief           | InjectDLL into Win*32 Process RING 3
-\ /
-/ \ Licence         | Ce code est totalement libre.
-\ /                 | Je vous encourage à le partager et/ou le modifier.
-/ \                 | Un grand pouvoir implique de grandes responsabilités.
-\*/
+#include <stdio.h>
+#include <windows.h>
+#include <TlHelp32.h>
 
-    #include <iostream>
-    #include <cstring>
-    #include <windows.h>
-    #include <TlHelp32.h>
+int GetProcessPidByName(char *nProcess);
+HANDLE GetHandleDll(char *PathDll, int pid);
 
-    using namespace std;
+int main(int argc, char *argv[]) {
+	printf("InjectDLL into Win*32 Process RING 3 ~ follow @tfairane");
+	if (argc != 3) {
+		printf("Usage : %s <process name> <full path DLL>", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    int GetProcessPidByName( char *nProcess);
-    HANDLE GetHandleDll( char *PathDll, int pid);
+	char *PathDll = argv[2]; // Specifie FULL Path to DLL !!!!
+	int sPathDll = strlen(PathDll);
+	int pid = GetProcessPidByName(argv[1]);
 
-    int main( int argc, char *argv[])
-    {
-        cout << "InjectDLL into Win*32 Process RING 3 | follow @tfairane" << endl;
-        if(argc!=3) {
-            cout << "Usage : " << argv[0] << "[Process Name] [Full Path Dll]" << endl;
-            exit(EXIT_FAILURE);
-        }
+	printf("[~] Target Process Name : %s PID : %d", argv[1], pid);
 
-        char *PathDll   =   argv[2];// Specifie FULL Path to DLL !!!!
-        int sPathDll    =   strlen(PathDll);
-        int pid         =   GetProcessPidByName(argv[1]);
+	HANDLE hProcess = OpenProcess( PROCESS_ALL_ACCESS, FALSE, pid);
+	if (hProcess == NULL) {
+		printf("[#] OpenProcess() : Error"), exit(EXIT_FAILURE);
+	}
 
-        cout << "[Target] " << endl << "Name : " << argv[1] << endl << "PID : " << pid << endl;
+	LPVOID AddrAlloc = VirtualAllocEx(hProcess, NULL, sPathDll, MEM_COMMIT,
+	PAGE_EXECUTE_READWRITE);
+	if (AddrAlloc == NULL) {
+		printf("[#] VirtualAllocEx() : Error");
+		exit(EXIT_FAILURE);
+	}
 
-        HANDLE hProcess     =   OpenProcess( PROCESS_ALL_ACCESS, FALSE, pid);
-        if(hProcess == NULL) {
-            cout << "[#] OpenProcess() : Error (" << GetLastError() << ")" << endl;
-            exit(EXIT_FAILURE);
-        }
+	if (WriteProcessMemory(hProcess, AddrAlloc, PathDll, sPathDll, 0) == 0) {
+		printf("[#] WriteProcessMemory() : Error");
+		exit(EXIT_FAILURE);
+	}
 
-        LPVOID AddrAlloc    =   VirtualAllocEx( hProcess, NULL, sPathDll, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-        if(AddrAlloc == NULL) {
-            cout << "[#] VirtualAllocEx() : Error (" << GetLastError() << ")" << endl;
-            exit(EXIT_FAILURE);
-        }
+	LPTHREAD_START_ROUTINE addrLoadLibrary =
+			(LPTHREAD_START_ROUTINE) GetProcAddress(
+			GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+	if (addrLoadLibrary == NULL) {
+		printf(
+				"[#] GetProcAddress(\"kernel32.dll\", \"LoadLibraryA\") : Error");
+		exit(EXIT_FAILURE);
+	}
 
-        if(WriteProcessMemory( hProcess, AddrAlloc, PathDll, sPathDll, 0) == 0) {
-            cout << "[#] WriteProcessMemory() : Error (" << GetLastError() << ")" << endl;
-            exit(EXIT_FAILURE);
-        }
+	HANDLE RemoteFunction = CreateRemoteThread(hProcess, NULL, 0,
+			addrLoadLibrary, AddrAlloc, 0, 0);
+	if (RemoteFunction == NULL) {
+		printf("[#] CreateRemoteThread() : Error");
+		exit(EXIT_FAILURE);
+	}
 
-        LPTHREAD_START_ROUTINE addrLoadLibrary = (LPTHREAD_START_ROUTINE) GetProcAddress( GetModuleHandle("kernel32.dll"), "LoadLibraryA");
-        if(addrLoadLibrary == NULL) {
-            cout << "[#] GetProcAddress(\"kernel32.dll\", \"LoadLibraryA\") : Error (" << GetLastError() << ")" << endl;
-            exit(EXIT_FAILURE);
-        }
+	printf("[~] DLL Injected ! :o");
+	WaitForSingleObject(RemoteFunction, 1000);
+	VirtualFreeEx(hProcess, AddrAlloc, 0, MEM_DECOMMIT);
+	CloseHandle(RemoteFunction);
 
-        HANDLE RemoteFunction   =   CreateRemoteThread( hProcess, NULL, 0, addrLoadLibrary, AddrAlloc, 0, 0);
-        if(RemoteFunction == NULL) {
-            cout << "[#] CreateRemoteThread() : Error (" << GetLastError() << ")" << endl;
-            exit(EXIT_FAILURE);
-        }
+	LPTHREAD_START_ROUTINE addrFreeLibrary =
+			(LPTHREAD_START_ROUTINE) GetProcAddress(
+			GetModuleHandle("kernel32.dll"), "FreeLibrary");
+	if (addrFreeLibrary == NULL) {
+		printf("[#] GetProcAddress(\"kernel32.dll\", \"FreeLibrary\") : Error");
+		exit(EXIT_FAILURE);
+	}
 
-        cout << "[~] DLL Injected ! :o" << endl;
-        WaitForSingleObject( RemoteFunction, 1000);
-        VirtualFreeEx( hProcess, AddrAlloc, 0, MEM_DECOMMIT);
-        CloseHandle( RemoteFunction);
+	RemoteFunction = CreateRemoteThread(hProcess, NULL, 0, addrFreeLibrary,
+			GetHandleDll(PathDll, pid), 0, 0);
+	if (RemoteFunction == NULL) {
+		printf("[#] CreateRemoteThread() : Error");
+		exit(EXIT_FAILURE);
+	}
+	printf("[~] DLL Free ! ;)");
+	CloseHandle(RemoteFunction);
+	CloseHandle(hProcess);
 
-        LPTHREAD_START_ROUTINE addrFreeLibrary = (LPTHREAD_START_ROUTINE) GetProcAddress( GetModuleHandle("kernel32.dll"), "FreeLibrary");
-        if(addrFreeLibrary == NULL) {
-            cout << "[#] GetProcAddress(\"kernel32.dll\", \"FreeLibrary\") : Error (" << GetLastError() << ")" << endl;
-            exit(EXIT_FAILURE);
-        }
+	return 0;
+}
 
-        RemoteFunction = CreateRemoteThread( hProcess, NULL, 0, addrFreeLibrary, GetHandleDll(PathDll, pid), 0 , 0);
-        if(RemoteFunction == NULL) {
-            cout << "[#] CreateRemoteThread() : Error (" << GetLastError() << ")" << endl;
-            exit(EXIT_FAILURE);
-        }
+int GetProcessPidByName(char *nProcess) {
+	PROCESSENTRY32 ProcessEntry = { 0 };
+	ProcessEntry.dwSize = sizeof(PROCESSENTRY32);
+	HANDLE Snapshot32Process = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0);
+	Process32First(Snapshot32Process, &ProcessEntry);
+	while (Process32Next(Snapshot32Process, &ProcessEntry)
+			&& strcmp(ProcessEntry.szExeFile, nProcess) != 0)
+		;
+	CloseHandle(Snapshot32Process);
 
-        cout << "[~] DLL Free ! ;)" << endl;
-        CloseHandle(RemoteFunction);
-        CloseHandle(hProcess);
+	if (!strcmp(ProcessEntry.szExeFile, nProcess))
+		return ProcessEntry.th32ProcessID;
+	else
+		return -1;
+}
 
-        return 0;
-    }
+HANDLE GetHandleDll(char *PathDll, int pid) {
+	MODULEENTRY32 ModuleEntry = { 0 };
+	ModuleEntry.dwSize = sizeof(MODULEENTRY32);
+	HANDLE Snapshot32Module = CreateToolhelp32Snapshot( TH32CS_SNAPMODULE, pid);
+	Module32First(Snapshot32Module, &ModuleEntry);
 
-    int GetProcessPidByName( char *nProcess) {
-        PROCESSENTRY32 ProcessEntry = { 0 };
-        ProcessEntry.dwSize = sizeof(PROCESSENTRY32);
-        HANDLE Snapshot32Process = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0);
-        Process32First( Snapshot32Process, &ProcessEntry);
-        while(Process32Next( Snapshot32Process, &ProcessEntry) && strcmp( ProcessEntry.szExeFile, nProcess)!=0);
-        CloseHandle( Snapshot32Process);
-
-        if(!strcmp( ProcessEntry.szExeFile, nProcess))
-            return ProcessEntry.th32ProcessID;
-        else
-            return -1;
-    }
-
-    HANDLE GetHandleDll( char *PathDll, int pid) {
-        MODULEENTRY32 ModuleEntry = { 0 };
-        ModuleEntry.dwSize = sizeof(MODULEENTRY32);
-        HANDLE Snapshot32Module = CreateToolhelp32Snapshot( TH32CS_SNAPMODULE, pid);
-        Module32First( Snapshot32Module, &ModuleEntry);
-
-        while(Module32Next( Snapshot32Module, &ModuleEntry));
-        CloseHandle( Snapshot32Module);
-        if(!strstr( ModuleEntry.szModule, PathDll))
-            return ModuleEntry.hModule;
-        else
-            return INVALID_HANDLE_VALUE;
-    }
+	while (Module32Next(Snapshot32Module, &ModuleEntry))
+		;
+	CloseHandle(Snapshot32Module);
+	if (!strstr(ModuleEntry.szModule, PathDll))
+		return ModuleEntry.hModule;
+	else
+		return INVALID_HANDLE_VALUE ;
+}
