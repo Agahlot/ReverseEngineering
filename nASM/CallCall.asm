01cae288e9eb8667eb545bf562f0ab3d61a4fa44
AiraneToufik <tf.airane@gmail.com>
2014-04-23 18:03:29 +0200

    nASM

---
+++ nASM/CallCall.asm
@@ -0,0 +1,12 @@
+global _main
+
+SECTION .text
+next:
+mov eax, 0x7635c5b9 ;Hard Offset 'printf' msvcrt.dll
+call eax
+add esp, 4
+ret 0x0
+
+_main:
+call next
+db "Hello World", 0
---
+++ nASM/PushPush.asm
@@ -0,0 +1,13 @@
+global _main
+extern _printf
+
+SECTION .text
+_main:
+push 00646c72h
+push 6f57206fh
+push 6c6c6548h
+mov eax, esp ;ASCII "Hello World"
+push eax
+call _printf
+add esp, 16
+ret 0x0
