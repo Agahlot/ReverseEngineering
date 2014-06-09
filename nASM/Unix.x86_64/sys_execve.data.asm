ef119562259f86b4464194e013a06631911399e9
AiraneToufik <tf.airane@gmail.com>
2014-06-10 00:41:07 +0200

    nASM

--- nASM/Unix.x86_64/sys_execve.data.asm
+++ nASM/Unix.x86_64/sys_execve.data.asm
@@ -6,12 +6,12 @@ shell db '/bin/sh', 0
 section .text
 _start:
 
-mov rax, 59
-mod rdi, shell
+mov rax, 59 ; sys_execve
+mov rdi, shell
 mov rsi, 0
 mov rdx, 0
 syscall
 
-mov rax, 60
+mov rax, 60 ; sys_exit
 mov rdi, 0
 syscall
