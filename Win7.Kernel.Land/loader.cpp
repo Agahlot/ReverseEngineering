27e14bf97f619f920f133c00b89286b1979286b3
AiraneToufik <tf.airane@gmail.com>
2014-08-06 10:27:28 +0100

    Kernel Land
    Come back

---
+++ Win7.Kernel.Land/loader.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include <Windows.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	SC_HANDLE hSCManager;
+	SC_HANDLE hService;
+	SERVICE_STATUS ss;
+
+	hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CREATE_SERVICE);
+
+	if(hSCManager) {
+
+		hService = CreateService(
+			hSCManager,
+			TEXT("DriverTestServer"),
+			TEXT("DriverTestServer"),
+			SERVICE_START | DELETE | SERVICE_STOP,
+			SERVICE_KERNEL_DRIVER,
+			SERVICE_DEMAND_START,
+			SERVICE_ERROR_IGNORE,
+			TEXT(""),
+			NULL, NULL, NULL, NULL, NULL);
+
+		if(!hService) {
+
+			hService = OpenService(
+				hSCManager,
+				TEXT("DriverTestServer"),
+				SERVICE_START | DELETE | SERVICE_STOP);
+		}
+
+		if(hService) {
+
+			StartService(hService, 0, NULL);
+
+			printf("Press Enter to close service\r\n");
+			getchar();
+
+			ControlService(hService, SERVICE_CONTROL_STOP, &ss);
+			DeleteService(hService);
+			CloseServiceHandle(hService);
+		}
+		CloseServiceHandle(hSCManager);
+	}
+
+	return 0;
+}
+
