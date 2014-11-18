1a3e252430bef2909af8e2a42321ad64d8641561
AiraneToufik <tf.airane@gmail.com>
2014-11-18 02:29:54 +0100

    rootkit

--- Kernel.Land/Windows/loader.cpp
+++ Kernel.Land/Windows/loader.cpp
@@ -6,35 +6,40 @@ int _tmain(int argc, _TCHAR* argv[])
 	SC_HANDLE hSCManager;
 	SC_HANDLE hService;
 	SERVICE_STATUS ss;
+	
+	printf("Load W7 driver unsigned/signed\nhigh privileged required\n");
 
+	// get Service Control Manager
 	hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CREATE_SERVICE);
 
-	if(hSCManager) {
+	if (hSCManager) {
 
+		// Create Service
 		hService = CreateService(
 			hSCManager,
-			TEXT("DriverTestService"),
-			TEXT("DriverTestService"),
+			TEXT("POC.DKOM.W7.rootkit"),
+			TEXT("POC.DKOM.W7.rootkit"),
 			SERVICE_START | DELETE | SERVICE_STOP,
 			SERVICE_KERNEL_DRIVER,
 			SERVICE_DEMAND_START,
 			SERVICE_ERROR_IGNORE,
-			TEXT(""),
+			TEXT("C:\\\\Users\\Airane\\Documents\\Visual Studio 2013\\Projects\\rootkit\\x64\\Win7Debug\\rootkit.sys"),
 			NULL, NULL, NULL, NULL, NULL);
 
-		if(!hService) {
-
+		// if service does not exist, open it 
+		if (!hService) {
 			hService = OpenService(
 				hSCManager,
-				TEXT("DriverTestService"),
+				TEXT("POC.DKOM.W7.rootkit"),
 				SERVICE_START | DELETE | SERVICE_STOP);
 		}
 
-		if(hService) {
+		// if service exist, start it
+		if (hService) {
 
 			StartService(hService, 0, NULL);
 
-			printf("Press Enter to close service\r\n");
+			printf("Please, press any key to unload driver\n");
 			getchar();
 
 			ControlService(hService, SERVICE_CONTROL_STOP, &ss);
@@ -42,6 +47,10 @@ int _tmain(int argc, _TCHAR* argv[])
 			CloseServiceHandle(hService);
 		}
 		CloseServiceHandle(hSCManager);
+	} else {
+		// if "5 : ERROR_ACCESS_DENIED" : check permission
+		printf("Error : %d", GetLastError());
+		return 1;
 	}
 
 	return 0;
