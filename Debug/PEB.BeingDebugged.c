5d1f365ca62dbfb6bfb4aa4f471ef2e45722e3e1
Toufik Airane <toufik.airane@gmail.com>
2015-07-20 07:30:25 +0200

    Update PEB.BeingDebugged.c

--- Debug/PEB.BeingDebugged.c
+++ Debug/PEB.BeingDebugged.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-#include<PEB.h>
+#include<windows.h>
+#include<winternl.h>
 
 typedef DWORD (WINAPI*pNtQueryInformationProcess)(HANDLE, enum PROCESSINFOCLASS,
 		PVOID, ULONG, PULONG);
